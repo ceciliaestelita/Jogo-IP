8c28495df4db764194ea753cfb16070d64c8cc24
ceciliaestelita <106453269+ceciliaestelita@users.noreply.github.com>
2025-12-17 00:06:50 -0300

    Código dos 3 coletáveis

---
+++ Estrela.py
@@ -0,0 +1,42 @@
+import pygame
+import random
+
+class Estrela(pygame.sprite.Sprite):
+    tamanho_px = 30
+    def __init__(self, largura_tela, altura_tela, tela):
+        super().__init__()
+
+        # Carrega a sprite 
+        self.image = pygame.image.load(
+            "Sprites/estrela.png"
+        ).convert_alpha()
+
+        
+        self.vida = 1
+        
+        self.image = pygame.transform.scale(self.image, (self.tamanho_px, self.tamanho_px))
+
+        self.rect = self.image.get_rect()
+
+        # Posição inicial aleatória no topo
+        self.rect.x = random.randint(0, largura_tela - self.rect.width)
+        self.rect.y = random.randint(-100, -40)
+
+        # Movimento
+        
+        self.vel = random.uniform(5.6, 7.4)
+        
+        self.altura_tela = altura_tela
+        self.tela = tela
+
+    def update(self):
+        self.rect.y += self.vel
+
+    def desenhar(self):
+        self.tela.blit(self.image, self.rect)
+
+    def fora_da_tela(self):
+        return self.rect.top > self.altura_tela
+
+
+
